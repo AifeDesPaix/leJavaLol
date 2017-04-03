6bc9f43218c00a5dcf80fa585a8ea45a7b43ac3f
Joan <joan.tassel@orange.fr>
2017-04-03 14:52:40 +0200

    baze

--- Bonjour.java
+++ Bonjour.java
@@ -1,6 +1,6 @@
 public class Bonjour {
 
-    public static String message = "Jew";
+    private static String message = "Jew";
 
     public static void main(String[] args) {
         System.out.println(message);
